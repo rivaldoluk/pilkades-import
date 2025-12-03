04963b1487d648b881f31e1ac1cfcfc484dad452
rivaldoluk <134913863+rivaldoluk@users.noreply.github.com>
2025-12-03 20:00:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 // public/config.js
 export default {
   CONTRACT_ADDRESS: "0xc8A27EbF3D1dBFacEE7F3C43edeD5B2e1469d886",
-  RPC_URL: "https://eth-sepolia.g.alchemy.com/v2/jiM9nsjGmstv8jUal4qKx",
+  RPC_URL: "https://eth-sepolia.g.alchemy.com/v2/demo",
   RELAYER_URL: "http://localhost:3000/public/vote",
   HASIL_URL: "http://localhost:3000/public/hasil",
   SEPOLIA_CHAIN_ID: "0xaa36a7",
   DURASI_6_JAM: 6 * 3600
 };
+
